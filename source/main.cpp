222462b532e851a1ebfe47e5e4400178a745447f
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-10-01 02:29:01 +0200

    project base

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "utilities.h"
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+int main() {
+    // world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1),0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
+    // camera
+    camera cam;
+    cam.aspect_ratio = 16.0/9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.render(world);
+    return 0;
+}
