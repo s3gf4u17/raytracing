e908b232a09f84494085556700e20a9438c991ab
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-11-13 22:35:30 +0100

    cuda test

---
+++ test.cu
@@ -0,0 +1,255 @@
+#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}
+#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}#include <stdio.h>
+
+__global__ void cuda_hello() {
+    printf("hello gpu\n");
+}
+
+int main() {
+    printf("hello cpu\n");
+    cuda_hello<<<1,1>>>();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    return 0;
+}
